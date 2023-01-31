ea897e3ef45c5c235bb1254446ab0a39c9caec5e
Hyun Jeong Shin <hj0304hj@gmail.com>
2023-01-31 09:05:40 +0900

    2023 01 30 19:21
    오류 수정

--- src/views/ProvideInject.vue
+++ src/views/ProvideInject.vue
@@ -3,7 +3,7 @@
         <child-inject></child-inject>
         <h1 v-if="tf === true">부모는 제어</h1>
         <button @click="close">클릭</button>
-        <h1>{{ text }}</h1>
+        <!-- <h1>{{ text }}</h1> -->
     </div>
     <!-- => 계층 구조가 복잡하다고 해도 부모 데이터를 자식에게 쉽게 보낼 수 있음 -->
     <!-- => 단점: 어떤 부모한테서 데이터를 받았는지 확인하기 힘듬 -->
@@ -14,6 +14,7 @@
 import ChildInject from '@/components/ChildInject.vue';
 
 export default {
+    
     data () {
         
 
@@ -25,7 +26,8 @@ export default {
     components: {
         ChildInject
     },
-    method: {
+    // method는 오타임...
+    methods: {
         close() {
             // 버튼으로 부모 보였다 사라졌다 하기
             if(this.tf === true) {
@@ -33,12 +35,13 @@ export default {
             } else {
                 return this.tf = true
             }
-        },
-        provid() {
-            return{
-                textmsg: this.text,
-                tfmsg: this.tf
-            }
+        }
+    },
+    // provide는 methods 밖에...
+    provide() {
+        return{
+            textmsg: this.text,
+            tfmsg: this.tf
         }
     }
 }
